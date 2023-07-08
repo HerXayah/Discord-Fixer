31e6d7ede4568ea6ff9a1f464f10b927d6bab12f
Sarah Engel <justsweetluna@gmail.com>
2023-07-08 12:41:47 +0200

    Commit Commiting. Shit works now...

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 set compiler=%1
 if "%1"=="" set compiler=gcc.exe
 cd "%~dp0"
-"%compiler%" -s -mwindows src/main.c -lwtsapi32 -o DiscordFixer.exe
-upx --best --ultra-brute DiscordFixer.exe>nul 2>&1
+"%compiler%" -s -mwindows src/main.c -lwtsapi32 -o ./output/DiscordFixer.exe
+mkdir output>nul 2>&1
+upx --best --ultra-brute /output/DiscordFixer.exe>nul 2>&1
