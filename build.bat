0a959cc807c3915f41eb35ced302b9cc54ed95ac
Sarah Engel <justsweetluna@gmail.com>
2023-07-08 16:24:22 +0200

    Added Icon

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
-gcc.exe -s -mwindows WinMain.c -lpathcch -lntdll -lwtsapi32 -lshlwapi -o DiscordFixer.exe
+windres icon.rc -O coff -o icon.res
+gcc.exe -s -mwindows WinMain.c -lpathcch -lntdll -lwtsapi32 -lshlwapi icon.res -o DiscordFixer.exe
 upx --best --ultra-brute DiscordFixer.exe>nul 2>&1
