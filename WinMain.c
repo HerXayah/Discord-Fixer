95a4a2a69997c1306526022a33a7d2fd97419f83
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2023-12-14 16:26:15 +0100

    Optimize readability and speed

--- WinMain.c
+++ WinMain.c
@@ -9,43 +9,41 @@ BOOL FindDiscordExe(const wchar_t* folderPattern, wchar_t* resultPath) {
     WIN32_FIND_DATAW findData;
     HANDLE hFind = FindFirstFileW(folderPattern, &findData);
 
-    while (hFind != INVALID_HANDLE_VALUE) {
+    if (hFind == INVALID_HANDLE_VALUE) {
+        return FALSE;
+    }
+
+    do {
         if ((findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) &&
             wcscmp(findData.cFileName, L".") != 0 &&
             wcscmp(findData.cFileName, L"..") != 0) {
             _snwprintf(resultPath, MAX_PATH, L"%s\\discord.exe", findData.cFileName);
+            FindClose(hFind);
             return TRUE;
         }
+    } while (FindNextFileW(hFind, &findData));
 
-        if (!FindNextFileW(hFind, &findData)) {
-            FindClose(hFind);
-            hFind = INVALID_HANDLE_VALUE;
-        }
-    }
-
+    FindClose(hFind);
     return FALSE;
 }
 
-int main() {
-    wchar_t discordPath[MAX_PATH];
-    if (!FindDiscordExe(L"app-*", discordPath)) {
-        return 0;
-    }
-
-    // Rest of your existing code
+void SuspendDiscordThread(const wchar_t* discordPath) {
     WTS_PROCESS_INFOW* pProcessInfo = NULL;
     DWORD dwCount = 0;
     BOOL bSuspended = FALSE;
 
     ShellExecuteW(NULL, NULL, discordPath, NULL, NULL, SW_SHOWNORMAL);
+
     while (!bSuspended && !SleepEx(1, TRUE)) {
         WTSEnumerateProcessesW(WTS_CURRENT_SERVER, 0, 1, &pProcessInfo, &dwCount);
+
         for (DWORD dwIndex = 0; dwIndex < dwCount; dwIndex++) {
             if (_wcsicmp(discordPath, _wcslwr(pProcessInfo[dwIndex].pProcessName)) == 0)
                 continue;
 
             HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-            THREADENTRY32 te = {.dwSize = sizeof(THREADENTRY32)};
+            THREADENTRY32 te = { sizeof(THREADENTRY32) };
+
             if (Thread32First(hThreadSnapshot, &te)) {
                 do {
                     if (te.th32OwnerProcessID != pProcessInfo[dwIndex].ProcessId)
@@ -56,7 +54,8 @@ int main() {
                     NtQueryInformationThread(hThread, ThreadQuerySetWin32StartAddress, &dw64StartAddress, sizeof(DWORD64), NULL);
 
                     HANDLE hModuleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE32 | TH32CS_SNAPMODULE, pProcessInfo[dwIndex].ProcessId);
-                    MODULEENTRY32W me = {.dwSize = sizeof(MODULEENTRY32W)};
+                    MODULEENTRY32W me = { sizeof(MODULEENTRY32W) };
+
                     if (Module32FirstW(hModuleSnapshot, &me)) {
                         do {
                             if (dw64StartAddress >= (DWORD64)me.modBaseAddr &&
@@ -65,13 +64,24 @@ int main() {
                                 bSuspended = SuspendThread(hThread);
                         } while (Module32NextW(hModuleSnapshot, &me));
                     }
+
                     CloseHandle(hModuleSnapshot);
                     CloseHandle(hThread);
                 } while (Thread32Next(hThreadSnapshot, &te));
             }
+
             CloseHandle(hThreadSnapshot);
         }
+
         WTSFreeMemory(pProcessInfo);
+    }
+}
+
+int main() {
+    wchar_t discordPath[MAX_PATH];
+
+    if (FindDiscordExe(L"app-*", discordPath)) {
+        SuspendDiscordThread(discordPath);
     }
 
     return 0;
