1ce524c5ce4b6fdef8de08bbad2b79e41671719c
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2023-12-16 19:49:52 +0100

    Update gitignore Update build script

--- build.bat
+++ build.bat
@@ -1,4 +1,13 @@
 @echo off
-windres icon.rc -O coff -o icon.res
-gcc.exe -s -mwindows WinMain.c -lpathcch -lntdll -lwtsapi32 -lshlwapi icon.res -o DiscordFixer.exe
-upx --best --ultra-brute DiscordFixer.exe>nul 2>&1
+set SOURCE_FILE=WinMain.c
+set OUTPUT_FILE=DiscordFixer.exe
+set RESOURCE_FILE=icon.rc
+
+rem Compile resource file
+windres %RESOURCE_FILE% -O coff -o icon.res
+
+rem Compile source file with optimized GCC flags
+gcc.exe -O3 -Wall -s -mwindows %SOURCE_FILE% -lpathcch -lntdll -lwtsapi32 -lshlwapi icon.res -o %OUTPUT_FILE%
+
+rem Compress executable with UPX
+upx --best --ultra-brute %OUTPUT_FILE% > nul 2>&1
